4b664759b494fd1e8d2a2460d7265a58783ee0a7
Vikram Jadhav <vikramcse.10@gmail.com>
2020-01-25 16:55:57 +0000

    added configuration in sales-admin and created logging object in one place and passed it in handler

--- cmd/sales-admin/main.go
+++ cmd/sales-admin/main.go
@@ -3,41 +3,73 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/ardanlabs/conf"
+	"github.com/pkg/errors"
 	"github.com/vikramcse/the-service/internal/platform/database"
 	"github.com/vikramcse/the-service/internal/schema"
 )
 
 func main() {
+	if err := run(); err != nil {
+		log.Printf("error: shutting down: %s", err)
+		os.Exit(1)
+	}
+}
 
-	flag.Parse()
+func run() error {
+	var cfg struct {
+		DB struct {
+			User       string `conf:"default:postgres"`
+			Password   string `conf:"default:postgres,noprint"`
+			Host       string `conf:"default:localhost"`
+			Name       string `conf:"default:postgres"`
+			DisableTLS bool   `conf:"default:false"`
+		}
+		Args conf.Args
+	}
+
+	if err := conf.Parse(os.Args[1:], "SALES", &cfg); err != nil {
+		if err == conf.ErrHelpWanted {
+			usage, err := conf.Usage("SALES", &cfg)
+			if err != nil {
+				return errors.Wrap(err, "generating usage")
+			}
+			fmt.Println(usage)
+			return nil
+		}
+		return errors.Wrap(err, "error: parsing config")
+	}
 
 	// Initialize dependencies.
-	db, err := database.Open()
+	db, err := database.Open(database.Config{
+		User:       cfg.DB.User,
+		Password:   cfg.DB.Password,
+		Host:       cfg.DB.Host,
+		Name:       cfg.DB.Name,
+		DisableTLS: cfg.DB.DisableTLS,
+	})
 	if err != nil {
-		log.Fatalf("error: connecting to db: %s", err)
+		return errors.Wrap(err, "connecting to db")
 	}
 	defer db.Close()
 
-	switch flag.Arg(0) {
+	switch cfg.Args.Num(0) {
 	case "migrate":
 		if err := schema.Migrate(db); err != nil {
-			log.Println("error applying migrations", err)
-			os.Exit(1)
+			return errors.Wrap(err, "applying migrations")
 		}
 		fmt.Println("Migrations complete")
-		return
 
 	case "seed":
 		if err := schema.Seed(db); err != nil {
-			log.Println("error seeding database", err)
-			os.Exit(1)
+			return errors.Wrap(err, "seeding database")
 		}
 		fmt.Println("Seed data complete")
-		return
 	}
+
+	return nil
 }
